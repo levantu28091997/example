315cbbb2db13d095bbb2115bfedf6418fb181599
anh tu <levantu.nd1997@gmail.com>
2019-10-01 17:35:32 +0700

    up

--- wp-content/themes/homepage/core/init.php
+++ wp-content/themes/homepage/core/init.php
@@ -5,3 +5,5 @@ require_once('/custom-post-type.php');
 require_once('/customizer.php');
 require_once('/class-tgm-plugin-activation.php');
 require_once('/plugins.php');
+require_once('/options.php');
+
